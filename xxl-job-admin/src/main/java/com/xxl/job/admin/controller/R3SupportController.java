355468075556f93466f01f8779e8ebe9dbee853c
Weasley <weasley2023@outlook.com>
2025-05-11 19:46:28 +0800

    feat: add user group permissions endpoint
    Added new endpoint to fetch job group permissions via XxlJobGroupDao

--- xxl-job-admin/src/main/java/com/xxl/job/admin/controller/R3SupportController.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/controller/R3SupportController.java
@@ -1,7 +1,10 @@
 package com.xxl.job.admin.controller;
 
+import com.xxl.job.admin.core.model.XxlJobGroup;
+import com.xxl.job.admin.dao.XxlJobGroupDao;
 import com.xxl.job.admin.service.XxlJobService;
 import com.xxl.job.core.biz.model.ReturnT;
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +22,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class R3SupportController {
     @Autowired
     private XxlJobService xxlJobService;
+    @Autowired
+    private XxlJobGroupDao xxlJobGroupDao;
 
+    /**
+     * 图表数据
+     */
     @GetMapping("/job/report/overview")
     public ReturnT<Map<String, Object>> getJobReportOverview() {
         Map<String, Object> info = xxlJobService.dashboardInfo();
         return new ReturnT<>(info);
     }
+
+    /**
+     * 用户组权限(执行器)
+     */
+    @GetMapping("/user/group/permissions")
+    public ReturnT<List<XxlJobGroup>> getUserGroupPermissions() {
+        List<XxlJobGroup> groupList = xxlJobGroupDao.findAll();
+        return new ReturnT<>(groupList);
+    }
+
 }
