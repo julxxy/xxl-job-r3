f0d9a655d82e8cfbf34ee72d343a6de3ec51ec2e
julian <julxxy@outlook.com>
2025-08-05 17:49:42 +0800

    refactor: update logging configuration and application properties for R3 support

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -4,13 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author xuxueli 2018-10-28 00:38:13
+ * XXL-JOB Admin Application
+ *
+ * @author xuxueli,Julian
+ * @date 2018-10-28 00:38:13
  */
 @SpringBootApplication
 public class XxlJobAdminApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(XxlJobAdminApplication.class, args);
-	}
+    }
 
 }
